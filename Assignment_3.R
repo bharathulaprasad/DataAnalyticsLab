60bea260f9a0d0f31ad96dfb4ab61e0f57fb6b7b
Krishna Prasad <76819369+bharathulaprasad@users.noreply.github.com>
2021-09-23 11:53:16 +0200

    Update Assignment_3.R

--- Assignment_3.R
+++ Assignment_3.R
@@ -1,8 +1,16 @@
-#1
+#1a
 library(datasets)
 barplot(iris$Petal.Length)
 
-#2
+#1b
 library("RColorBrewer")
 barplot(iris$Sepal.Length, col=brewer.pal(3,"Set1"))
 barplot(table(iris$Species, iris$Petal.Length), col=brewer.pal(3,"Set1"))
+
+#1c
+data(iris)
+par(mfrow=c(2,2))
+boxplot(iris$Sepal.Length,col="red")
+boxplot(iris$Sepal.Length~iris$Species,col="red")
+boxplot(iris$Sepal.Length~iris$Species,col=heat.colors(3))
+boxplot(iris$Sepal.Length~iris$Species,col=topo.colors(3))
